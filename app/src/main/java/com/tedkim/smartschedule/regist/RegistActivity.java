cce4fdd40dab6927511abab849e3f84cc410b7b8
Ted <kimtx2017@gmail.com>
2017-08-07 05:27:46 +0900

    Add widget
    - add date/time picker dialog in RegistActivity

--- app/src/main/java/com/tedkim/smartschedule/regist/RegistActivity.java
+++ app/src/main/java/com/tedkim/smartschedule/regist/RegistActivity.java
@@ -1,5 +1,7 @@
 package com.tedkim.smartschedule.regist;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,9 +9,12 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.tedkim.smartschedule.R;
 import com.tedkim.smartschedule.model.ScheduleData;
@@ -22,14 +27,19 @@ public class RegistActivity extends AppCompatActivity implements View.OnClickLis
     Toolbar mToolbar;
     ImageButton mBack, mSave;
     EditText mTitle, mDesc, mAddress, mContacts;
-    TextView mCalendarDate, mStart, mEnd, mNotification;
+    TextView mDate, mStart, mEnd, mNotification;
     CheckBox mAllDay, mFakeCall;
 
     // realm database instance
-    private Realm mRealm;
+    Realm mRealm;
 
     // date info from Home Activity
-    private String mDate;
+    String mDateInfo, mStartInfo, mEndInfo;
+
+    private static final int SET_START = 0;
+    private static final int SET_END = 1;
+
+    int mTimeset = SET_START;
 
     @Override
     protected void onStart() {
@@ -52,19 +62,19 @@ public class RegistActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
 
-        mDate = getIntent().getStringExtra("DATE");
+        mDateInfo = getIntent().getStringExtra("DATE");
 
-        Log.d("CHECK_ENTER","Register Activity -------------------");
-        Log.d("CHECK_DATE", "In register >>>>>>>>>>>>>>>>"+mDate);
+        Log.d("CHECK_ENTER", "Register Activity -------------------");
+        Log.d("CHECK_DATE", "In register >>>>>>>>>>>>>>>>" + mDateInfo);
 
         initView();
     }
 
-    public void initView(){
+    public void initView() {
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar_regist);
         setSupportActionBar(mToolbar);
-        mToolbar.setContentInsetsAbsolute(0,0);
+        mToolbar.setContentInsetsAbsolute(0, 0);
 
         mBack = (ImageButton) findViewById(R.id.imageButton_back);
         mBack.setOnClickListener(this);
@@ -75,9 +85,9 @@ public class RegistActivity extends AppCompatActivity implements View.OnClickLis
         mTitle = (EditText) findViewById(R.id.editText_title);
         mDesc = (EditText) findViewById(R.id.editText_desc);
 
-        mCalendarDate = (TextView) findViewById(R.id.textView_date);
-        mCalendarDate.setText(mDate);
-        mCalendarDate.setOnClickListener(this);
+        mDate = (TextView) findViewById(R.id.textView_date);
+        mDate.setText(mDateInfo);
+        mDate.setOnClickListener(this);
 
         mStart = (TextView) findViewById(R.id.textView_start);
         mStart.setOnClickListener(this);
@@ -98,26 +108,42 @@ public class RegistActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View v) {
 
-        switch(v.getId()){
+        // TODO - 각각 현재 날짜와 시간 가져오는 방법을 알아 볼 것
+        DatePickerDialog dateDialog = new DatePickerDialog(this, mDateListener, 2017, 8 - 1, 7);
+        TimePickerDialog timeDialog = new TimePickerDialog(this, mTimeListener, 15, 24, false);
+
+        switch (v.getId()) {
 
             case R.id.imageButton_back:
+
                 finish();
+
                 break;
 
             case R.id.imageButton_save:
+
                 insertSchedule();
                 finish();
+
                 break;
 
             case R.id.textView_date:
+
+                dateDialog.show();
 
                 break;
 
             case R.id.textView_start:
 
+                mTimeset = SET_START;
+                timeDialog.show();
+
                 break;
 
             case R.id.textView_end:
+
+                mTimeset = SET_END;
+                timeDialog.show();
 
                 break;
 
@@ -127,7 +153,49 @@ public class RegistActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
-    private void insertSchedule(){
+    private DatePickerDialog.OnDateSetListener mDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+            monthOfYear = monthOfYear + 1;
+            mDateInfo = year + "-" + monthOfYear + "-" + dayOfMonth;
+            mDate.setText(mDateInfo);
+        }
+    };
+
+    private TimePickerDialog.OnTimeSetListener mTimeListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+            String am_pm;
+
+            if(hourOfDay < 12) {
+                am_pm = "AM";
+            } else {
+                if(hourOfDay!=12){
+                    hourOfDay = hourOfDay - 12;
+                }
+                am_pm = "PM";
+            }
+
+            if(mTimeset == SET_START){
+                mStartInfo = am_pm +" "+ hourOfDay + ":"+minute;
+                mStart.setText(mStartInfo);
+            }
+            else{
+
+                mEndInfo = am_pm +" "+ hourOfDay + ":"+minute;
+                mEnd.setText(mEndInfo);
+            }
+
+            // 설정버튼 눌렀을 때
+            Toast.makeText(getApplicationContext(), hourOfDay + "시 " + minute + "분", Toast.LENGTH_SHORT).show();
+
+        }
+    };
+
+
+    private void insertSchedule() {
 
 //        if(isEmptyEditors(mTitle) || isEmptyEditors(mDesc) || isEmptyEditors(mLocation) || isEmptyEditors(mStart) || isEmptyEditors(mEnd)){
 //
@@ -140,16 +208,15 @@ public class RegistActivity extends AppCompatActivity implements View.OnClickLis
             @Override
             public void execute(Realm realm) {
 
-                ScheduleData newSchedule = mRealm.createObject(ScheduleData.class,getNextKey());
+                ScheduleData newSchedule = mRealm.createObject(ScheduleData.class, getNextKey());
 
-                newSchedule.setDate(mDate);
                 newSchedule.setTitle(mTitle.getText().toString());
                 newSchedule.setDesc(mDesc.getText().toString());
-//                newSchedule.setLocation();
-//                newSchedule.setContacts();
 
-                newSchedule.setStartTime(mStart.getText().toString());
-                newSchedule.setEndTime(mEnd.getText().toString());
+                newSchedule.setDate(mDateInfo);
+
+                newSchedule.setStartTime(mStartInfo);
+                newSchedule.setEndTime(mEndInfo);
 
                 newSchedule.setAddress(mAddress.getText().toString());
 
@@ -168,10 +235,9 @@ public class RegistActivity extends AppCompatActivity implements View.OnClickLis
         });
     }
 
-
     // check valid about editTexts
     private boolean isEmptyEditors(EditText view) {
-        if(TextUtils.isEmpty(view.getText().toString())) {
+        if (TextUtils.isEmpty(view.getText().toString())) {
             view.setError("잘좀 써라");
             view.requestFocus();
             return true;
